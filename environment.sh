ce478bb56a3e14b333db49705ace14b2f1108194
James Hawley <jamesrichardhawley@gmail.com>
2020-01-08 00:13:26 -0500

    Updating env.sh with updated python, macs2

--- environment.sh
+++ environment.sh
@@ -1,21 +1,21 @@
-conda create -n ATACseq
+conda create -n ATACseq 
 conda activate ATACseq
 
-# no dependencies
-conda install -c bioconda sambamba samtools
+conda config --add channels r
+conda config --add channels bioconda
+conda config --add channels conda-forge
 
 # some dependencies
-conda install bioconda::bedtools=2.23.0
-conda install bioconda::cutadapt
-conda install bioconda::fastqc
-conda install bioconda::bowtie2
-conda install bioconda::snakemake-minimal
-conda install pandas
+conda install sambamba samtools
+conda install trim-galore fastqc
+conda install bowtie2
+conda install python>=3.6
+conda install pandas scipy snakemake-minimal
 conda install pysam
+conda install bedtools pybedtools
+conda install r-dunn.test
+conda install macs2>=2.2.4
 
 # bioconductor
-conda install -c bioconda bioconductor-edger bioconductor-diffbind bioconductor-deseq2
+conda install bioconductor-edger bioconductor-diffbind bioconductor-deseq2
 
-conda install pybedtools
-conda install scipy
-conda install r-dunn.test
