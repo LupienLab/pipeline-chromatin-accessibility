a84e99e4aa8f1e9d1f99784a4a37bb06e37cd656
James Hawley <jamesrichardhawley@gmail.com>
2020-01-07 23:18:55 -0500

    Initial commit
    * Including conda environment packages and versions

---
+++ environment.sh
@@ -0,0 +1,21 @@
+conda create -n ATACseq
+conda activate ATACseq
+
+# no dependencies
+conda install -c bioconda sambamba samtools
+
+# some dependencies
+conda install bioconda::bedtools=2.23.0
+conda install bioconda::cutadapt
+conda install bioconda::fastqc
+conda install bioconda::bowtie2
+conda install bioconda::snakemake-minimal
+conda install pandas
+conda install pysam
+
+# bioconductor
+conda install -c bioconda bioconductor-edger bioconductor-diffbind bioconductor-deseq2
+
+conda install pybedtools
+conda install scipy
+conda install r-dunn.test
