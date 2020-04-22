41694183d1dc77bad1268862099d62665e284603
nandankita <nandankita@gmail.com>
2020-04-22 18:56:17 -0400

    changes

---
+++ run/environment.sh
@@ -0,0 +1,20 @@
+conda create -n ATACseq 
+conda activate ATACseq
+
+conda config --add channels r
+conda config --add channels bioconda
+conda config --add channels conda-forge
+
+# some dependencies
+conda install sambamba samtools
+conda install trim-galore fastqc
+conda install bowtie2
+conda install python>=3.6
+conda install pandas scipy snakemake-minimal
+conda install pysam
+conda install bedtools pybedtools
+conda install r-dunn.test
+conda install macs2>=2.2.4
+
+# bioconductor
+conda install bioconductor-edger bioconductor-diffbind bioconductor-deseq2
---
+++ run/run-pipeline.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH -p long
+#SBATCH --job-name pipeline
+#SBATCH -t 7-00:00:00
+#SBATCH -o log.out
+#SBATCH -e log.err
+
+ 
+snakemake -j 10 -c "sbatch {cluster.params}" -u cluster.yaml --latency-wait 30
