33fd667c2a3f69f127fa24fa5de16373ec0374ee
nandankita <nandankita@gmail.com>
2020-06-25 19:18:48 -0400

    granges

---
+++ pipeline/R/Bed_To_GRange.R
@@ -0,0 +1,30 @@
+rm(list = ls())
+library(data.table)
+library(GenomicRanges)
+###
+reference_name <- "TEonly_repeats_hg38"
+file_name_pattern <- "bed"
+reference_path <- '../pipeline/static/TEonly_repeats_hg38/'
+out_dir <- './'
+
+reference_files <- list.files(reference_path, pattern = file_name_pattern)
+###
+Grange_reference_set <- list()
+for(SamIter in 1:length(reference_files)){ 
+  #print(SamIter)
+  query_mat2 <- fread(paste(reference_path, reference_files[SamIter],
+                            sep = "", collapse = ""),
+                      select = c(1:3), data.table=FALSE)
+  ##########
+  gr <- GRanges(seqnames=Rle(query_mat2$V1),
+                ranges = IRanges(query_mat2$V2, end=query_mat2$V3))
+  ##########
+  Grange_reference_set[[SamIter]] <- gr
+}
+
+names(Grange_reference_set) <- reference_files
+
+saveRDS(Grange_reference_set,
+        file = paste(out_dir, reference_name,
+                     "_AllPeaks_Granges.RDS",
+                     sep = "", collapse = ""))
