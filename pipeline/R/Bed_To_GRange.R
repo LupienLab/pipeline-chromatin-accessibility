c22b267a75b5a7d256a2f3e87766f123473d440c
nandankita <nandankita@gmail.com>
2020-06-26 16:06:27 -0400

    repeats

--- pipeline/R/Bed_To_GRange.R
+++ pipeline/R/Bed_To_GRange.R
@@ -1,30 +1,59 @@
 rm(list = ls())
+
+####Library######################################
+library(optparse)
 library(data.table)
 library(GenomicRanges)
-###
-reference_name <- "TEonly_repeats_hg38"
-file_name_pattern <- "bed"
-reference_path <- '../static/TEonly_repeats_hg38/'
-out_dir <- './'
 
-reference_files <- list.files(reference_path, pattern = file_name_pattern)
-###
-Grange_reference_set <- list()
-for(SamIter in 1:length(reference_files)){ 
-  #print(SamIter)
-  query_mat2 <- fread(paste(reference_path, reference_files[SamIter],
-                            sep = "", collapse = ""),
-                      select = c(1:3), data.table=FALSE)
-  ##########
-  gr <- GRanges(seqnames=Rle(query_mat2$V1),
-                ranges = IRanges(query_mat2$V2, end=query_mat2$V3))
-  ##########
-  Grange_reference_set[[SamIter]] <- gr
+####User Input######################################
+option_list = list(
+  make_option(c("-f", "--narrowfile"), type="character", default=NULL,
+              help="Narrow peaks file", metavar="character"),
+  make_option(c("-r", "--repeats"), type="character", default=NULL,
+              help="Repeats directory", metavar="character"),
+  
+);
+opt_parser = OptionParser(option_list=option_list);
+opt = parse_args(opt_parser);
+
+if (is.null(opt$narrowfile) || is.null(opt$repeats)){
+  print_help(opt_parser)
+  stop("No arguments given.", call.=FALSE)
 }
 
-names(Grange_reference_set) <- reference_files
+#####################################################
 
-saveRDS(Grange_reference_set,
-        file = paste(out_dir, reference_name,
-                     "_AllPeaks_Granges.RDS",
-                     sep = "", collapse = ""))
+print(1)
+
+#------------------------------------------------
+narrowPeaksFile <- opt$narrowfile
+rep_folder_dir <- opt$repeats
+
+peaks <- fread(narrowPeaksFile, header = F)
+colnames(peaks) <- c('seqnames', 'start', 'end')
+
+rep_paths <- list.files(rep_folder_dir, full.names = T)
+
+query_ranges <- GRanges(seqnames = peaks$seqnames, ranges = IRanges(start = peaks$start, end = peaks$end))
+
+print(2)
+
+for(this_rep_path in rep_paths){
+  this_rep <- fread(this_rep_path)
+  colnames(this_rep) <- c('seqnames', 'start', 'end')
+  
+  #remove rows with bad seqnames
+  this_rep <- this_rep[which(this_rep$seqnames %in% paste0('chr',c(1:22,'X','Y')))]
+  
+  this_rep_name <- gsub('.bed.sorted', '', rev(strsplit(this_rep_path,'\\/')[[1]])[1])
+  this_rep_ranges <- GRanges(seqnames = this_rep$seqnames, ranges = IRanges(start = this_rep$start, end = this_rep$end))
+  
+  ovlps <- as.data.table(findOverlaps(query = query_ranges, subject = this_rep_ranges))
+  
+  bm_for_repeatElements[ , this_rep_name] <- 0
+  bm_for_repeatElements[unique(ovlps$queryHits), this_rep_name] <- 1
+}
+
+print(3)
+
+saveRDS(bm_for_repeatElements, 'bm_for_repeatElements.rds')
