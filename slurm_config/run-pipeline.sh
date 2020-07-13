f6ec0071acff8084cc83fa5cbccc0a05401862a4
ankita <ankita@10.0.0.188>
2020-07-13 15:40:56 -0400

    changes

--- slurm_config/run-pipeline.sh
+++ slurm_config/run-pipeline.sh
@@ -4,6 +4,5 @@
 #SBATCH -t 7-00:00:00
 #SBATCH -o log.out
 #SBATCH -e log.err
-#SBATCH --IMAGE=../run/rb-atac_pipeline_latest.sif
 
 snakemake -j 10 -c "sbatch {cluster.params}" -u ../slurm_config/slurm.yaml --latency-wait 30  --nolock --use-singularity
